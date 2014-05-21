0e02ae2c02f24e1058b188095f253f68c83d11fe
nishimura.takeshi <nishimura.takeshi@zeevaa.com>
2014-05-21 12:14:47 +0900

    「cake bake all」で各テーブル毎に作成

---
+++ php_project001/app/Test/Fixture/GroupFixture.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * GroupFixture
+ *
+ */
+class GroupFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'MyISAM')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'name' => 'Lorem ipsum dolor sit amet',
+			'created' => '2014-05-21 12:12:02',
+			'modified' => '2014-05-21 12:12:02'
+		),
+	);
+
+}
